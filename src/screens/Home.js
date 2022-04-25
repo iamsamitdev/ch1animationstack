eecc5cdee1c7f3b7e2a1bd8166c0a49c60c37282
Samit Koyom <samitkoyom@gmail.com>
2022-04-25 14:38:43 +0700

    update home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontSize:24}}>Home Screen</Text>
+    </SafeAreaView>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'center',
+    alignItems: 'center',
+    backgroundColor:'lightgreen'
+  }
+})
